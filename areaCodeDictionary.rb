ecf7ec4a7ada5a94c20a63cce0a173bcfe38d59d
Bas <suckling.w.s@gmail.com>
2020-01-22 10:08:29 +1300

    complete area code dictionary problem

--- areaCodeDictionary.rb
+++ areaCodeDictionary.rb
@@ -13,16 +13,35 @@ dial_book = {
  
 # Get city names from the hash
 def get_city_names(somehash)
-# Write code here
+  somehash.keys
 end
  
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
-# Write code here
+  somehash.each do |cityname| 
+    if cityname.first == key
+      return cityname.last
+    end
+  end
+  puts "unknown error"
 end
+
  
 # Execution flow
 loop do
     puts "Do you want to look up an area code by city name? (Y/N)"
-    answer = gets.chomp.lower
+    answer = gets.chomp.downcase
+    puts
+    break if answer != 'y'
+    puts "Which city do you want the area code for?"
+    puts
+    puts get_city_names(dial_book)
+    puts
+    puts "Enter your selection"
+    city = gets.chomp.downcase
+    if dial_book.include?(city)
+      puts get_area_code(dial_book, city)
+    else
+      puts "Invalid input"
+    end
 end
